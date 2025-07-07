d1b95d56bfbed3230ac62de78da935ce30380e03
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-07-07 13:41:39 +0200

    Fichier react et vite

---
+++ front/src/MockData.js
@@ -0,0 +1,24 @@
+export const booksMock = [
+  {
+    _id: "1",
+    titre: "Le Petit Prince",
+    auteur: "Antoine de Saint-Exupéry",
+    couverture: "https://covers.openlibrary.org/b/id/8226191-L.jpg",
+    etat: "à lire",
+    pages: 96,
+    categorie: "Classique",
+    dernierePageLue: 0,
+    favori: false,
+  },
+  {
+    _id: "2",
+    titre: "1984",
+    auteur: "George Orwell",
+    couverture: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+    etat: "en cours",
+    pages: 328,
+    dernierePageLue: 100,
+    categorie: "Dystopie",
+    favori: true,
+  },
+];
